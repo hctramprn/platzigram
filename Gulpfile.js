860665d4257badb7a71d5c96d753777e04032cec
Héctor Amparán <hctramprn@users.noreply.github.com>
2017-06-02 23:14:23 -0500

    Creación del archivo index.scss que será utilizado por gulp para unir nuestro código de estilos anterior al de la librería de materialize. Modificación del archivo Gulpfile.js al cual se le asignan las tareas de manipular los archivos de estilos. Se descargó el paquete gulp-rename para modificar el archivo de salida de la tarea de gulp y se agregó la dependencia a package.json

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,2 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+gulp.task('styles', function() {
+  gulp
+    .src(index.scss)
+    .pipe(sass())
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['styles'])
